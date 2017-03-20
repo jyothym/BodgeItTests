707de443084bc8ee90581ee6e9096cd7dcbcfd06
Jyothy Mariam John <jyothyj@injyothj.local>
2017-03-20 13:25:01 +0530

    Refactoring

--- src/tw/java/securitytests/SpiderAndActiveScan.java
+++ src/tw/java/securitytests/SpiderAndActiveScan.java
@@ -3,6 +3,7 @@ package tw.java.securitytests;
 import org.zaproxy.clientapi.core.ApiResponse;
 import org.zaproxy.clientapi.core.ApiResponseElement;
 import org.zaproxy.clientapi.core.ClientApi;
+import org.zaproxy.clientapi.core.ClientApiException;
 
 /**
  * A simple example showing how to use the API to spider and active scan a site and then retrieve and print out the alerts.
@@ -18,57 +19,74 @@ public class SpiderAndActiveScan {
 	private static final String TARGET = "http://localhost:8080/bodgeit/";
 
 	public static void main(String[] args) {
+
 		ClientApi api = new ClientApi(ZAP_ADDRESS, ZAP_PORT, ZAP_API_KEY);
-
 		try {
-			// Start spidering the target
-			System.out.println("Spider : " + TARGET);
-			// It's not necessary to pass the ZAP API key again, already set when creating the ClientApi.
-			ApiResponse resp = api.spider.scan(TARGET, null, null, null, null);
-			String scanid;
-			int progress;
 
-			// The scan now returns a scan id to support concurrent scanning
-			scanid = ((ApiResponseElement) resp).getValue();
-
-			// Poll the status until it completes
-			while (true) {
-				Thread.sleep(1000);
-				progress = Integer.parseInt(((ApiResponseElement) api.spider.status(scanid)).getValue());
-				System.out.println("Spider progress : " + progress + "%");
-				if (progress >= 100) {
-					break;
-				}
-			}
-			System.out.println("Spider complete");
-
-			// Give the passive scanner a chance to complete
-			Thread.sleep(2000);
-
-			System.out.println("Active scan : " + TARGET);
-			resp = api.ascan.scan(TARGET, "True", "False", null, null, null);
-
-			// The scan now returns a scan id to support concurrent scanning
-			scanid = ((ApiResponseElement) resp).getValue();
-
-			// Poll the status until it completes
-			while (true) {
-				Thread.sleep(5000);
-				progress = Integer.parseInt(((ApiResponseElement) api.ascan.status(scanid)).getValue());
-				System.out.println("Active Scan progress : " + progress + "%");
-				if (progress >= 100) {
-					break;
-				}
-			}
-			System.out.println("Active Scan complete");
-
-			System.out.println("Alerts:");
-			System.out.println(new String(api.core.xmlreport()));
+			startSpidering(api);
+			startActiveScan(api);
+			generateXMLReport(api);
 
 		} catch (Exception e) {
 			System.out.println("Exception : " + e.getMessage());
 			e.printStackTrace();
 		}
+	}
+
+	private static void generateXMLReport(ClientApi api)
+			throws ClientApiException {
+		System.out.println("Alerts:");
+		System.out.println(new String(api.core.xmlreport()));
+	}
+
+	private static void startSpidering(ClientApi api)
+			throws ClientApiException, InterruptedException {
+		// Start spidering the target
+		System.out.println("Spider : " + TARGET);
+		// It's not necessary to pass the ZAP API key again, already set when creating the ClientApi.
+		ApiResponse resp = api.spider.scan(TARGET, null, null, null, null);
+		String scanid;
+		int progress;
+
+		// The scan now returns a scan id to support concurrent scanning
+		scanid = ((ApiResponseElement) resp).getValue();
+
+		// Poll the status until it completes
+		while (true) {
+			Thread.sleep(1000);
+			progress = Integer.parseInt(((ApiResponseElement) api.spider.status(scanid)).getValue());
+			System.out.println("Spider progress : " + progress + "%");
+			if (progress >= 100) {
+				break;
+			}
+		}
+		System.out.println("Spider complete");
+
+		// Give the passive scanner a chance to complete
+		Thread.sleep(2000);
+	}
+
+	private static void startActiveScan(ClientApi api)
+			throws ClientApiException, InterruptedException {
+		ApiResponse resp;
+		String scanid;
+		int progress;
+		System.out.println("Active scan : " + TARGET);
+		resp = api.ascan.scan(TARGET, "True", "False", null, null, null);
+
+		// The scan now returns a scan id to support concurrent scanning
+		scanid = ((ApiResponseElement) resp).getValue();
+
+		// Poll the status until it completes
+		while (true) {
+			Thread.sleep(5000);
+			progress = Integer.parseInt(((ApiResponseElement) api.ascan.status(scanid)).getValue());
+			System.out.println("Active Scan progress : " + progress + "%");
+			if (progress >= 100) {
+				break;
+			}
+		}
+		System.out.println("Active Scan complete");
 	}
 
 }
--- src/tw/java/utils/BaseTest.java
+++ src/tw/java/utils/BaseTest.java
@@ -12,24 +12,20 @@ import org.zaproxy.clientapi.gen.Core;
 
 import tw.java.pages.BodgeIt;
 
-
 public class BaseTest extends TestListenerAdapter {
 	public WebDriver driver;
 	public BodgeIt homePage;
 	public String browser = "firefox";
-	private static final String ZAP_ADDRESS = "localhost";
-	private static final int ZAP_PORT = 8090;
-	private static final String ZAP_API_KEY = null; // Change this if you have set the apikey in ZAP via Options / API
-	ClientApi clientApi = new ClientApi(ZAP_ADDRESS, ZAP_PORT, ZAP_API_KEY);
+	ClientApi clientApi = new ClientApi("localhost", 8090, null);
 	Core core = new Core(clientApi);
 	CreateHTMLReport c = new CreateHTMLReport();
-	
+
 	@BeforeClass
 	public void intializeDriver(Object[] arg0) throws Exception {
-		new ProcessBuilder("/Applications/OWASP ZAP.app/Contents/MacOS/OWASP ZAP.sh").start(); //, "-daemon", "-port" + ZAP_PORT
 		System.out.println("Starting ZAP...");
+		new ProcessBuilder("/Applications/OWASP ZAP.app/Contents/MacOS/OWASP ZAP.sh").start(); //, "-daemon", "-port" + ZAP_PORT
 		Thread.sleep(10000);
-		
+
 		driver = BrowserFactory.getBrowser(browser);
 		driver.get("http://localhost:8080/bodgeit/");
 	}
@@ -37,23 +33,19 @@ public class BaseTest extends TestListenerAdapter {
 	@BeforeMethod
 	public void beforeMethod() throws Exception {
 		homePage = new BodgeIt(driver);
-
 	}
 
 	@AfterClass
 	public void closeBrowser() throws ClientApiException{
 		driver.manage().deleteAllCookies();
 		driver.quit();
-		
-//		System.out.println("XML report output");
-//      String alerts_report = new String(core.xmlreport());
 
 		System.out.println("Generating HTML Report...");
-        String alerts_report = new String(core.htmlreport());
-        c.generateHTMLFile(alerts_report);
-        
-        System.out.println("Shutting down ZAP...");
-        clientApi.core.shutdown();
+		String alerts_report = new String(core.htmlreport());
+		c.generateHTMLFile(alerts_report);
+
+		System.out.println("Shutting down ZAP...");
+		clientApi.core.shutdown();
 	}
 
 	@AfterSuite
